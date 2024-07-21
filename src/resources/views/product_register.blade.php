e886cd0768fa1404c9821ca20b2d65e884fe8cd0
甲木志緒里 <shootingstar.531@icloud.com>
2024-07-21 22:40:54 +0900

    コードの修正

--- src/resources/views/product_register.blade.php
+++ src/resources/views/product_register.blade.php
@@ -13,6 +13,7 @@
         <div class="container-title">
             <h1>商品登録</h1>
         </div>
+
         <div class="form-item">
             <p class="form-item-label">
                 商品名<span class="form-item-label-required">必須</span>
@@ -24,6 +25,7 @@
                 @enderror
             </div>
         </div>
+
         <div class="form-item">
             <p class="form-item-label">
                 値段<span class="form-item-label-required">必須</span>
@@ -35,6 +37,7 @@
                 @enderror
             </div>
         </div>
+
         <div class="form-item">
             <p class="form-item-label">
                 商品画像<span class="form-item-label-required">必須</span>
@@ -46,6 +49,7 @@
                 @enderror
             </div>
         </div>
+
         <div class="form-item">
             <p class="form-item-label">
                 季節<span class="form-item-label-required">必須</span>
@@ -67,6 +71,7 @@
                 @enderror
             </div>
         </div>
+
         <div class="form-item">
             <p class="form-item-label">
                 商品説明<span class="form-item-label-required">必須</span>
@@ -78,6 +83,7 @@
                 @enderror
             </div>
         </div>
+
         <div class="button-container">
             <button type="button" class="form-btn__back" onclick="history.back()">戻る</button>
             <input type="submit" class="form-btn" value="登録" />
